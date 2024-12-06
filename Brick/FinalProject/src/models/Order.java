a548413d2b7d2cb70d58c6823fe0789d92cce10e
Alex Garcia <alexxelacr7@gmail.com>
2024-12-05 20:12:28 -0800

    adding code to repo

---
+++ Brick/FinalProject/src/models/Order.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.Date;
+
+public class Order {
+    private int orderId;
+    private int customerId;
+    private Date orderDate;
+    private double totalAmount;
+    private String orderStatus;
+
+    public Order(int orderId, int customerId, Date orderDate, double totalAmount, String orderStatus) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+        this.orderStatus = orderStatus;
+    }
+
+    // Getters and Setters
+    public int getOrderId() { return orderId; }
+    public void setOrderId(int orderId) { this.orderId = orderId; }
+
+    public int getCustomerId() { return customerId; }
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+
+    public Date getOrderDate() { return orderDate; }
+    public void setOrderDate(Date orderDate) { this.orderDate = orderDate; }
+
+    public double getTotalAmount() { return totalAmount; }
+    public void setTotalAmount(double totalAmount) { this.totalAmount = totalAmount; }
+
+    public String getOrderStatus() { return orderStatus; }
+    public void setOrderStatus(String orderStatus) { this.orderStatus = orderStatus; }
+}
